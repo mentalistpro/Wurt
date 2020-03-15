9ce83c6acbf378a3e1f3dde1a8a2e9e08cb92b0c
mentalistpro <mentalistpro@users.noreply.github.com>
2020-03-15 22:49:40 +1100

    Solve issue #4, #5, #9

--- modmain.lua
+++ modmain.lua
@@ -1,7 +1,7 @@
 PrefabFiles = 
 {
     "wurt",
-	"wurt_tentacle_warning"
+    "wurt_tentacle_warning"
 }
 
 Assets =
@@ -16,13 +16,13 @@ AddMinimapAtlas("minimap/wurt.xml")
 
 ------------------------------------------------------------------------------------------------
 
---//CONTENT//
---#1 Character
+--[[CONTENT]]
+--#1 AddModCharacter
 --#2 Tuning
---#3 Wurt perks
+--#3 Perks
 
 ------------------------------------------------------------------------------------------------
---#1 Character
+--#1 AddModCharacter
 
 local _G = GLOBAL
 local require = _G.require
@@ -32,10 +32,11 @@ _S.CHARACTER_TITLES.wurt = "The Half-Pint"
 _S.CHARACTER_NAMES.wurt = "Wurt"
 _S.CHARACTER_DESCRIPTIONS.wurt = "*At home in the swamp \n*Has big plans for Merm-kind \n*Is a vegetarian"
 _S.CHARACTER_QUOTES.wurt = "\"Mermfolk known for hos-per-tality, florp.\""
+
+table.insert(_G.CHARACTER_GENDERS.FEMALE, "wurt")
 _S.CHARACTERS.WURT = require "speech_wurt"
 _S.NAMES.WURT = "Wurt"
 
-table.insert(_G.CHARACTER_GENDERS.FEMALE, "wurt")
 AddModCharacter("wurt")
 
 ------------------------------------------------------------------------------------------------
@@ -52,7 +53,7 @@ TUNING.WURT_SANITY_KINGBONUS = 200
 TUNING.WURT_FISH_PRESERVER_RATE = 1/4
 
 ------------------------------------------------------------------------------------------------
---#3 Wurt perks
+--#3 Perks
 
 local GetClock = _G.GetClock
 local GetPlayer = _G.GetPlayer
@@ -62,142 +63,89 @@ local GROUND = _G.GROUND
 local RoadManager = _G.RoadManager
 local TheSim = _G.TheSim
 
---Wurt's movement speed calculation
+--//Replace LocoMotor:UpdateGroundSpeedMultiplier()
+--I've tried to make changes at prefabs, but it crashes the game. So I change the component instead.
+--You can't detect world tile and character at the same time when the game has just started.
 
 AddComponentPostInit("locomotor",
-	function(self)	
-		function self:UpdateGroundSpeedMultiplier()			
-			self.groundspeedmultiplier = 1
-			local ground = GetWorld()
-			local oncreep = ground ~= nil and ground.GroundCreep:OnCreep(self.inst.Transform:GetWorldPosition())
-			local x,y,z = self.inst.Transform:GetWorldPosition()
-			
-			if oncreep then
-				-- if this ever needs to happen when self.enablegroundspeedmultiplier is set, need to move the check for self.enablegroundspeedmultiplier above
-				if self.triggerscreep and not self.wasoncreep then
-					local triggered = ground.GroundCreep:GetTriggeredCreepSpawners(x, y, z)
-					for _,v in ipairs(triggered) do
-						v:PushEvent("creepactivate", {target = self.inst})
-					end
-					self.wasoncreep = true
-				end
-				self.groundspeedmultiplier = self.slowmultiplier
-			else
-				self.wasoncreep = false
-				if self.fasteronroad then
-					if RoadManager and RoadManager:IsOnRoad( x,0,z ) then
-						self.groundspeedmultiplier = self.fastmultiplier
-					elseif ground ~= nil then
-						local tile = ground.Map:GetTileAtPoint(x,0,z)		
-						if tile and tile == GROUND.ROAD then
-							self.groundspeedmultiplier = self.fastmultiplier
-						--Wurt has fastmultiplier on marsh ground
-						elseif tile and tile == GROUND.MARSH and self.inst.prefab == "wurt" then
-							self.groundspeedmultiplier = self.fastmultiplier					
-						end
-					end
-				end
-			end
-		end
-	end
+    function(self)  
+        function self:UpdateGroundSpeedMultiplier()         
+            self.groundspeedmultiplier = 1
+            local ground = GetWorld()
+            local oncreep = ground ~= nil and ground.GroundCreep:OnCreep(self.inst.Transform:GetWorldPosition())
+            local x,y,z = self.inst.Transform:GetWorldPosition()
+            
+            if oncreep then
+                if self.triggerscreep and not self.wasoncreep then
+                    local triggered = ground.GroundCreep:GetTriggeredCreepSpawners(x, y, z)
+                    for _,v in ipairs(triggered) do
+                        v:PushEvent("creepactivate", {target = self.inst})
+                    end
+                    self.wasoncreep = true
+                end
+                self.groundspeedmultiplier = self.slowmultiplier    
+            else
+                self.wasoncreep = false
+                if self.fasteronroad then
+                    if RoadManager and RoadManager:IsOnRoad( x,0,z ) then
+                        self.groundspeedmultiplier = self.fastmultiplier
+                        
+                    --//Wurt's walk speed bonus on marsh turfs
+                    elseif ground ~= nil then
+                        local tile = ground.Map:GetTileAtPoint(x,0,z)
+                        local tile_is_marsh = tile == GROUND.MARSH or tile == GROUND.TIDALMARSH
+                        
+                        if tile and tile == GROUND.ROAD then
+                            self.groundspeedmultiplier = self.fastmultiplier
+                        elseif tile and tile_is_marsh and self.inst.prefab == "wurt" then
+                            self.groundspeedmultiplier = self.fastmultiplier                    
+                        end
+                    --//END
+                    
+                    end
+                end
+            end
+        end
+    end
 )
 
---Wurt's sanity calculation
+--//Replace Sanity:GetMoistureDelta()
 
-local function isfish(inst)
-	inst:AddTag("fish")
-end
-
-AddPrefabPostInit("eel", isfish)
-AddPrefabPostInit("fish", isfish)
-AddPrefabPostInit("tropical_fish", isfish)
-
-local function isdeadfish(inst)
-	inst:AddTag("deadfish")
-	inst:AddComponent("dapperness")
-	inst.components.dapperness.dapperness = -TUNING.DAPPERNESS_LARGE/1.5
-end
-
-AddPrefabPostInit("fish_cooked", isdeadfish)
-AddPrefabPostInit("fish_raw_small", isdeadfish)
-AddPrefabPostInit("fish_raw_small_cooked", isdeadfish)
-AddPrefabPostInit("fishsticks", isdeadfish)
-AddPrefabPostInit("fishtacos", isdeadfish)
+local easing = _G.require("easing")
 
 AddComponentPostInit("sanity",
-	function(self)	
-		function self:Recalc(dt)
-			local total_dapperness = self.dapperness or 0
-			local mitigates_rain = false		
-			
-			for k,v in pairs (self.inst.components.inventory.equipslots) do
-				if v.components.dapperness then
-					total_dapperness = total_dapperness + v.components.dapperness:GetDapperness(self.inst)
-					if v.components.dapperness.mitigates_rain then
-						mitigates_rain = true
-					end
-				end		
-			end
-			
-			local dapper_delta = total_dapperness*TUNING.SANITY_DAPPERNESS
-			
-			local light_delta = 0
-			local lightval = self.inst.LightWatcher:GetLightValue()
-			
-			local day = GetClock():IsDay() and not GetWorld():IsCave()
-			
-			if day then 
-				light_delta = TUNING.SANITY_DAY_GAIN
-			else	
-				local highval = TUNING.SANITY_HIGH_LIGHT
-				local lowval = TUNING.SANITY_LOW_LIGHT
-
-				if lightval > highval then
-					light_delta =  TUNING.SANITY_NIGHT_LIGHT
-				elseif lightval < lowval then
-					light_delta = TUNING.SANITY_NIGHT_DARK
-				else
-					light_delta = TUNING.SANITY_NIGHT_MID
-				end
-
-				light_delta = light_delta*self.night_drain_mult
-			end
-			
-			local aura_delta = 0
-			local x,y,z = self.inst.Transform:GetWorldPosition()
-			local ents = TheSim:FindEntities(x,y,z, TUNING.SANITY_EFFECT_RANGE, nil, {"FX", "NOCLICK", "DECOR","INLIMBO"} )
-			for k,v in pairs(ents) do 
-				if v.components.sanityaura and v ~= self.inst then
-					local distsq = self.inst:GetDistanceSqToInst(v)
-					local aura_val = v.components.sanityaura:GetAura(self.inst)/math.max(1, distsq)
-					if aura_val < 0 then
-						aura_val = aura_val * self.neg_aura_mult
-					end
-
-					aura_delta = aura_delta + aura_val
-				end
-			end
-
-			local rain_delta = 0
-			if GetSeasonManager() and GetSeasonManager():IsRaining() and not mitigates_rain then
-				rain_delta = -TUNING.DAPPERNESS_MED*1.5* GetSeasonManager():GetPrecipitationRate()
-			end
-
-			--Wurt suffers 50% less penalty from being wet
-			if self.inst.prefab == "wurt" then
-				self.rate = (dapper_delta + light_delta + aura_delta + rain_delta)*0.5
-			else
-				self.rate = (dapper_delta + light_delta + aura_delta + rain_delta)	
-			end
-			
-			if self.custom_rate_fn then
-				self.rate = self.rate + self.custom_rate_fn(self.inst)
-			end
-
-			self:DoDelta(self.rate*dt, true)
-		end
-	end
+    function(self)  
+        function self:GetMoistureDelta()
+            if self.wetnessImmune then 
+                return 0 
+            else 
+                local m = self.inst.components.moisture
+                
+                --//Wurt suffers from 50% less wetness penalty
+                if self.inst.prefab == "wurt" then
+                    return easing.inSine(m:GetMoisture(), 0, TUNING.MOISTURE_SANITY_PENALTY_MAX / 2, m.moistureclamp.max)
+                else
+                    return easing.inSine(m:GetMoisture(), 0, TUNING.MOISTURE_SANITY_PENALTY_MAX, m.moistureclamp.max)               
+                end
+                --//END
+                
+            end 
+        end
+    end
 )
+
+--//Fish preserves longer in Wurt's inventory
+
+local function FishPreserver(inst, holder)
+    local holder = GetPlayer()
+    if holder.prefab == "wurt" then
+        inst.components.perishable:SetPerishTime(TUNING.PERISH_SUPERFAST*4)
+    end
+end
+
+AddPrefabPostInit("eel", FishPreserver)
+AddPrefabPostInit("fish", FishPreserver)
+AddPrefabPostInit("tropical_fish", FishPreserver)
 
 
 
--- scripts/prefabs/wurt.lua
+++ scripts/prefabs/wurt.lua
@@ -5,27 +5,31 @@ local assets =
     Asset("ANIM", "anim/player_idles_wurt.zip"),
     Asset("ANIM", "anim/wurt_powerup.zip"),
     Asset("ANIM", "anim/wurt.zip"),
-    --Asset("SOUND", "sound/wurt.fsb"),
+    Asset("SOUND", "sound/wurt.fsb"),
 }
 
 local prefabs =
 {
-	"wurt_tentacle_warning"
+    "wurt_tentacle_warning"
 }
 
 local start_inv =
 {
-	"disguisehat"
+    "disguisehat"
 }
 
 ------------------------------------------------------------------------------------------------
 
---//CONTENT//
---1. Stats
---2. Tentacle warning
+--[[CONTENT]]
+--#1 Character Stats
+--#2 Eating Stats
+--#3 Reading Stats
+--#4 Tentacle Warning
+--#5 Other perks
+--#6 fn()
 
 ------------------------------------------------------------------------------------------------
---1. Stats
+--#1 Character Stats
 
 local function UpdateStats(inst, maxhealth, maxhunger, maxsanity)
 
@@ -51,8 +55,8 @@ local function RoyalUpgrade(inst, silent)
     UpdateStats(inst, TUNING.WURT_HEALTH_KINGBONUS, TUNING.WURT_HUNGER_KINGBONUS, TUNING.WURT_SANITY_KINGBONUS)
 
     if not silent and not inst.royal then
-    	inst.royal = true
-    	inst.components.talker:Say(GetString(inst, "ANNOUNCE_KINGCREATED"))        
+        inst.royal = true
+        inst.components.talker:Say(GetString(inst, "ANNOUNCE_KINGCREATED"))        
         inst.sg:PushEvent("powerup_wurt")
         inst.SoundEmitter:PlaySound("dontstarve/characters/wurt/transform_to")
     end
@@ -62,7 +66,7 @@ local function RoyalDowngrade(inst, silent)
     UpdateStats(inst, TUNING.WURT_HEALTH, TUNING.WURT_HUNGER, TUNING.WURT_SANITY)
 
     if not silent and inst.royal then
-    	inst.royal = nil
+        inst.royal = nil
         inst.components.talker:Say(GetString(inst, "ANNOUNCE_KINGDESTROYED"))
         inst.sg:PushEvent("powerdown_wurt")
         inst.SoundEmitter:PlaySound("dontstarve/characters/wurt/transform_from")
@@ -70,7 +74,61 @@ local function RoyalDowngrade(inst, silent)
 end
 
 ------------------------------------------------------------------------------------------------
---2. Tentacle warning
+--#2 Eating Stats
+
+local function onPreEat(food)
+    if food and food.components.edible then       
+        if food.components.edible.foodtype == "SEEDS" or food.components.edible.foodtype == "VEGGIE" then            
+            if food.components.edible.healthvalue < 0 then
+                food.components.edible.healthvalue = 0 
+            end           
+            if food.components.edible.sanityvalue < 0 then
+                food.components.edible.sanityvalue = 0 
+            end       
+        end 
+    end
+end
+
+local function onEat(inst, food)    
+    if food and food.components.edible then        
+        local food_health = food.components.edible.healthvalue        
+        local food_hunger = food.components.edible.hungervalue
+        local food_sanity = food.components.edible.sanityvalue
+        
+        --Wurt receives bonus food stats (need tweak?)
+        if food.prefab == "durian" or food.prefab == "durian_cooked" then
+            inst.components.health.currenthealth  = inst.components.health.currenthealth + food_health *0.6     
+            inst.components.hunger.current = inst.components.hunger.current + food_hunger *0.6  
+            inst.components.sanity.current = inst.components.sanity.current + food_sanity *0.6              
+        elseif food.components.edible.foodtype == "SEEDS" or food.components.edible.foodtype == "VEGGIE" then
+            inst.components.health.currenthealth  = inst.components.health.currenthealth + food_health *0.33        
+            inst.components.hunger.current = inst.components.hunger.current + food_hunger *0.33     
+            inst.components.sanity.current = inst.components.sanity.current + food_sanity *0.33     
+        end
+    end
+end
+
+------------------------------------------------------------------------------------------------
+--#3 Reading Stats
+
+local function peruse_brimstone(inst)
+    inst.components.sanity:DoDelta(-TUNING.SANITY_LARGE)
+end
+local function peruse_birds(inst)
+    inst.components.sanity:DoDelta(TUNING.SANITY_HUGE)
+end
+local function peruse_tentacles(inst)
+    inst.components.sanity:DoDelta(TUNING.SANITY_HUGE)
+end
+local function peruse_sleep(inst)
+    inst.components.sanity:DoDelta(TUNING.SANITY_LARGE)
+end
+local function peruse_gardening(inst)
+    inst.components.sanity:DoDelta(-TUNING.SANITY_LARGE)
+end
+
+------------------------------------------------------------------------------------------------
+--#4 Tentacle Warning
 
 local function VecUtil_Length(p1_x, p1_z)
     return math.sqrt(p1_x * p1_x + p1_z * p1_z)
@@ -97,85 +155,100 @@ local function ErodeAway(inst, erode_time)
 end
 
 local function UpdateTentacleWarnings(inst)
-	if not inst.components.inventory ~= nil then
-		local old_warnings = {}
-		for t, w in pairs(inst._active_warnings) do
-			old_warnings[t] = w
-		end
+    if not inst.components.inventory ~= nil then
+        local old_warnings = {}
+        for t, w in pairs(inst._active_warnings) do
+            old_warnings[t] = w
+        end
 
-		local x, y, z = inst.Transform:GetWorldPosition()
-		local warn_dist = 15
-		local tentacles = TheSim:FindEntities(x, y, z, warn_dist, {"WORM_DANGER"})
-		for i, t in ipairs(tentacles) do
-			local p1x, p1y, p1z = inst.Transform:GetWorldPosition()
-			local p2x, p2y, p2z = t.Transform:GetWorldPosition()
-			local dist = VecUtil_Length(p1x - p2x, p1z - p2z)
+        local x, y, z = inst.Transform:GetWorldPosition()
+        local warn_dist = 15
+        local tentacles = TheSim:FindEntities(x, y, z, warn_dist, {"WORM_DANGER"})
+        for i, t in ipairs(tentacles) do
+            local p1x, p1y, p1z = inst.Transform:GetWorldPosition()
+            local p2x, p2y, p2z = t.Transform:GetWorldPosition()
+            local dist = VecUtil_Length(p1x - p2x, p1z - p2z)
 
-			if t.components.health ~= nil and not t.components.health:IsDead() then
-				if inst._active_warnings[t] == nil then
-					local fx = SpawnPrefab("wurt_tentacle_warning")
-					fx.entity:SetParent(t.entity)
-					inst._active_warnings[t] = fx
-				else
-					old_warnings[t] = nil
-				end
-			end
-		end
+            if t.components.health ~= nil and not t.components.health:IsDead() then
+                if inst._active_warnings[t] == nil then
+                    local fx = SpawnPrefab("wurt_tentacle_warning")
+                    fx.entity:SetParent(t.entity)
+                    inst._active_warnings[t] = fx
+                else
+                    old_warnings[t] = nil
+                end
+            end
+        end
 
-		for t, w in pairs(old_warnings) do
-			inst._active_warnings[t] = nil
-			if w:IsValid() then
-				ErodeAway(w, 0.5)
-			end
-		end
-	
-	elseif next(inst._active_warnings) ~= nil then
-		for t, w in pairs(inst._active_warnings) do
-			if w:IsValid() then
-				w:Remove()
-			end
-		end
-		inst._active_warnings = {}
-	end
+        for t, w in pairs(old_warnings) do
+            inst._active_warnings[t] = nil
+            if w:IsValid() then
+                ErodeAway(w, 0.5)
+            end
+        end
+    
+    elseif next(inst._active_warnings) ~= nil then
+        for t, w in pairs(inst._active_warnings) do
+            if w:IsValid() then
+                w:Remove()
+            end
+        end
+        inst._active_warnings = {}
+    end
 end
 
 local function DisableTentacleWarning(inst)
-	if inst.tentacle_warning_task ~= nil then
-		inst.tentacle_warning_task:Cancel()
-		inst.tentacle_warning_task = nil
-	end
-			
-	for t, w in pairs(inst._active_warnings) do
-		if w:IsValid() then
-			w:Remove()
-		end
-	end
-	inst._active_warnings = {}
+    if inst.tentacle_warning_task ~= nil then
+        inst.tentacle_warning_task:Cancel()
+        inst.tentacle_warning_task = nil
+    end
+            
+    for t, w in pairs(inst._active_warnings) do
+        if w:IsValid() then
+            w:Remove()
+        end
+    end
+    inst._active_warnings = {}
 end
 
 local function EnableTentacleWarning(inst)
-	if inst.tentacle_warning_task == nil then
-		inst.tentacle_warning_task = inst:DoPeriodicTask(0.1, UpdateTentacleWarnings)
-	end
+    if inst.tentacle_warning_task == nil then
+        inst.tentacle_warning_task = inst:DoPeriodicTask(0.1, UpdateTentacleWarnings)
+    end
 end
 
 ------------------------------------------------------------------------------------------------
+--#5 Other perks
 
-local function peruse_brimstone(inst)
-    inst.components.sanity:DoDelta(-TUNING.SANITY_LARGE)
+local function FishLover(inst)
+    local isfish =  inst.components.inventory:Has("fish",1) or 
+                    inst.components.inventory:Has("tropical_fish",1) or 
+                    inst.components.inventory:Has("eel",1)                  
+                    
+    local notfish = inst.components.inventory:Has("fish_raw_small",1) or 
+                    inst.components.inventory:Has("fish_raw_small_cooked",1) or 
+                    inst.components.inventory:Has("fish_cooked",1) or 
+                    inst.components.inventory:Has("fishsticks",1) or 
+                    inst.components.inventory:Has("fishtacos",1)    
+
+    if isfish then
+        inst.components.sanity.dapperness = 100/(30*10*6)
+    elseif notfish then
+        inst.components.sanity.dapperness = -100/(30*10*6)
+    else
+        inst.components.sanity.dapperness = 0
+    end             
 end
-local function peruse_birds(inst)
-    inst.components.sanity:DoDelta(TUNING.SANITY_HUGE)
+
+local function mermbuilderfn(inst)
+    if GetPlayer() then
+        inst.components.sanity:DoDelta(5)
+    end
+    GetPlayer().components.talker:Say(GetString(GetPlayer().prefab, "BUILD_MERMSTRUCTURES"))            
 end
-local function peruse_tentacles(inst)
-    inst.components.sanity:DoDelta(TUNING.SANITY_HUGE)
-end
-local function peruse_sleep(inst)
-    inst.components.sanity:DoDelta(TUNING.SANITY_LARGE)
-end
-local function peruse_gardening(inst)
-    inst.components.sanity:DoDelta(-TUNING.SANITY_LARGE)
-end
+
+------------------------------------------------------------------------------------------------
+--#6 fn()
 
 local function OnSave(inst, data)
     data.health_percent = inst.health_percent or inst.components.health:GetPercent()
@@ -199,125 +272,73 @@ local function OnPreLoad(inst, data)
     end
 end
 
-local function OnRespawn(inst)
-    if GetWorld().components.mermkingmanager and GetWorld().components.mermkingmanager:HasKing() then
-        inst.overrideskinmode = "powerup"
-    else
-        inst.overrideskinmode = nil
-    end
-end
-
-local function onPreEat(food)
-	if food and food.components.edible then       
-		if food.components.edible.foodtype == "SEEDS" or food.components.edible.foodtype == "VEGGIE" then            
-			if food.components.edible.healthvalue < 0 then
-				food.components.edible.healthvalue = 0 
-			end           
-			if food.components.edible.sanityvalue < 0 then
-				food.components.edible.sanityvalue = 0 
-			end       
-		end 
-	end
-end
-
-local function onEat(inst, food)    
-	if food and food.components.edible then        
-		local food_health = food.components.edible.healthvalue         
-		local food_hunger = food.components.edible.hungervalue       
-		local food_sanity = food.components.edible.sanityvalue     
-		
-		--Bonus stats from food items
-		if food.prefab == "durian" or food.prefab == "durian_cooked" then
-			inst.components.health.currenthealth  = inst.components.health.currenthealth + food_health *0.6		
-			inst.components.hunger.current = inst.components.hunger.current + food_hunger *0.6	
-			inst.components.sanity.current = inst.components.sanity.current + food_sanity *0.6		  		
-		elseif food.components.edible.foodtype == "SEEDS" or food.components.edible.foodtype == "VEGGIE" then
-			inst.components.health.currenthealth  = inst.components.health.currenthealth + food_health *0.33		
-			inst.components.hunger.current = inst.components.hunger.current + food_hunger *0.33		
-			inst.components.sanity.current = inst.components.sanity.current + food_sanity *0.33		
-		end
-	end
-end
-
-local function mermbuilderfn(inst)
-	if GetPlayer() then
-		inst.components.sanity:DoDelta(5)
-	end
-	GetPlayer().components.talker:Say(GetString(GetPlayer().prefab, "BUILD_MERMSTRUCTURES"))            
-end
-
-local function fishlover(inst)
-	if inst.components.inventory:Has("fish",1) then
-		inst.components.sanity.dapperness = 100/(30*10*6)
-	else
-		inst.components.sanity.dapperness = 0
-	end        		
-end
-
-
-
-------------------------------------------------------------------------------------------------
-
 local function fn(inst)
-    inst:AddTag("playermerm")			--Wurt is a player, just a tag, no function.
-    inst:AddTag("merm")					--Wurt is a merm.
-    inst:AddTag("mermbuilder")			--Wurt receives sanity when merm structure is built, different from DST due to Mermhouse Crafting mod, see IsMermBuilder in this file.
-    inst:AddTag("mermfluent")			--Wurt is fluent in merm langugage, see Befriendable Merm mod.
-    inst:AddTag("mermguard")			--Wurt is a merm guard, see Befriendable Merm mod.
-    inst:AddTag("wet")					--Wurt is a wet entity.
-    inst:AddTag("stronggrip")			--Wurt is immortal if boat sinks, see HasStrongGrip in this file.
-	
-	inst.components.eater.foodprefs = { "VEGGIE","SEEDS" }
-	inst.components.eater:SetOnEatFn(onEat)   
-	local self = inst.components.eater   
-	local old = self.Eat   
-	local crockfoods = require("preparedfoods")        
-	function self:Eat(food, force) -- rewriting the Eat event        
-		if crockfoods[food.prefab] then -- test if food is a crockpot food         
-			return old(self, food, force)       
-		end        
-		onPreEat(food)        
-		return old(self, food, force)    
-	end
-		
+    --//Tags
+    inst:AddTag("playermerm")   
+    inst:AddTag("merm")         --merms are neutral
+    inst:AddTag("mermbuilder")  --gain sanity by building merm structures
+    inst:AddTag("mermfluent")   --read merm quotes
+    inst:AddTag("mermguard")    --befriend with mermguards
+    inst:AddTag("wet")          
+    inst:AddTag("stronggrip")   
+    
+    --//Eating
+    inst.components.eater.foodprefs = { "VEGGIE","SEEDS" }
+    inst.components.eater:SetOnEatFn(onEat)   
+    local self = inst.components.eater   
+    local old = self.Eat   
+    local crockfoods = require("preparedfoods")        
+    function self:Eat(food, force) -- rewriting the Eat event        
+        if crockfoods[food.prefab] then -- test if food is a crockpot food         
+            return old(self, food, force)       
+        end        
+        onPreEat(food)        
+        return old(self, food, force)    
+    end
+    
+    --//Reading
     inst:AddComponent("reader")
     inst.peruse_brimstone = peruse_brimstone
     inst.peruse_birds = peruse_birds
     inst.peruse_tentacles = peruse_tentacles
     inst.peruse_sleep = peruse_sleep
     inst.peruse_gardening = peruse_gardening
-	
-	inst.mermbuilderfn = mermbuilderfn
+    
+    --//Resistant to moisture
+    inst:AddComponent("moisturelistener")
+    inst.components.moisturelistener.wetnessSpeed = 0.25
+    inst.components.moisturelistener.wetnessResistance = 0.5
 
-    inst:ListenForEvent("itemget", function() fishlover(inst) end)
-    inst:ListenForEvent("itemlose", function() fishlover(inst) end)
-	
-    inst:ListenForEvent("onmermkingcreated", function() RoyalUpgrade(inst) end, GetWorld())
-    inst:ListenForEvent("onmermkingdestroyed", function() RoyalDowngrade(inst) end, GetWorld())
-
-	inst._active_warnings = {}
-	EnableTentacleWarning(inst)
-	UpdateTentacleWarnings(inst)
-
+    --//Merm King
     if GetWorld().components.mermkingmanager and GetWorld().components.mermkingmanager:HasKing() then
-        inst:DoTaskInTime(0, function() RoyalUpgrade(inst) end)
+        inst:DoTaskInTime(0, RoyalUpgrade)
     else
-        inst:DoTaskInTime(0, function() RoyalDowngrade(inst) end)
+        inst:DoTaskInTime(0, RoyalDowngrade)
     end
-		
-	if IsDLCEnabled and IsDLCEnabled(3) then
-		local disguisehat_recipe = Recipe(
-		"disguisehat", 
-		{
-		Ingredient("twigs", 2), 
-		Ingredient("pigskin", 1), 
-		Ingredient("beardhair", 1)
-		}, 
-		RECIPETABS.DRESS, TECH.NONE, "common", "")
-		disguisehat_recipe.sortkey = 1
-	end
-		
-	inst.OnSave = OnSave
+    inst:ListenForEvent("onmermkingcreated", RoyalUpgrade, GetWorld())
+    inst:ListenForEvent("onmermkingdestroyed", RoyalDowngrade, GetWorld())
+    
+    --//Hamlet Mask
+    if IsDLCEnabled and IsDLCEnabled(3) then
+        local disguisehat_recipe = Recipe(
+        "disguisehat", 
+        {
+        Ingredient("twigs", 2), 
+        Ingredient("pigskin", 1), 
+        Ingredient("beardhair", 1)
+        }, 
+        RECIPETABS.DRESS, TECH.NONE, "common", "")
+        disguisehat_recipe.sortkey = 1
+    end
+    
+    --//Other perks
+    inst._active_warnings = {}
+    inst:ListenForEvent("locomote", EnableTentacleWarning)
+    inst:ListenForEvent("itemget", FishLover)
+    inst:ListenForEvent("itemlose", FishLover)
+    inst.mermbuilderfn = mermbuilderfn
+
+    inst.OnSave = OnSave
     inst.OnPreLoad = OnPreLoad
 end
 
