ebff5ec18c8c7d7686f4a9e703f6dd65a2a26cb7
mentalistpro <mentalistpro@users.noreply.github.com>
2020-03-20 01:00:13 +1100

    [WARNING] Fixing murf recipes and assets

--- modinfo.lua
+++ modinfo.lua
@@ -1,4 +1,4 @@
-version                     = "1.0" 
+version                     = "1.1" 
 name                        = "Wurt"
 description                 = "Wurt, ported from DST, a young female merm."
 author                      = "mentalistpro"
